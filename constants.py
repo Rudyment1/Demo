a16c0a6829fd8f45d09ac96b25f8ee82e518d7fc
Rudyment1 <dvojtenko969@gmail.com>
2026-06-05 15:47:56 +0900

    add constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Настройки подключения к базе данных PostgreSQL.
+# Замените значения на свои.
+
+db_name = 'DataBase'
+db_user = 'postgres'
+db_password = 'Daniiel6541'
+db_host = 'localhost'
+db_port = '5432'
